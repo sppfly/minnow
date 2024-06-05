0d3c213cfcb9193c911a3c8ec113cc6221089bd2
liuyi <liuyicn6@qq.com>
2024-06-05 14:30:57 +0200

    temp

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -1,17 +1,29 @@
 #include "reassembler.hh"
+#include <iostream>
+#include <ostream>
 
 using namespace std;
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
-  // Your code here.
-  (void)first_index;
-  (void)data;
-  (void)is_last_substring;
+  auto& writer = this->output_.writer();
+  // if the data is too large, drop it directly
+  if ( data.length() > ( writer.available_capacity() - this->bytes_pending_ ) ) {
+    return;
+  }
+  // if the data is expecting data, write it in the bytestream
+  if ( first_index == this->expecting_index_ ) {
+    writer.push( data );
+    if ( is_last_substring ) {
+      writer.close();
+    }
+  } else {
+    // if not, buffer it in the assembler
+    // how to buffer it? what data structure should I use?
+  }
 }
 
 uint64_t Reassembler::bytes_pending() const
 {
-  // Your code here.
-  return {};
+  return this->bytes_pending_;
 }
--- src/reassembler.hh
+++ src/reassembler.hh
@@ -1,12 +1,21 @@
 #pragma once
 
 #include "byte_stream.hh"
+#include <cstdint>
+#include <map>
+#include <string>
+#include <sys/types.h>
 
 class Reassembler
 {
 public:
   // Construct Reassembler to write into given ByteStream.
-  explicit Reassembler( ByteStream&& output ) : output_( std::move( output ) ) {}
+  explicit Reassembler( ByteStream&& output )
+    : output_( std::move( output ) )
+    , expecting_index_( 0 )
+    , bytes_pending_( 0 )
+    , pending_data_( std::map<uint64_t, std::string> {} )
+  {}
 
   /*
    * Insert a new substring to be reassembled into a ByteStream.
@@ -42,4 +51,8 @@ public:
 
 private:
   ByteStream output_; // the Reassembler writes to this ByteStream
+  // my fields
+  uint64_t expecting_index_;
+  uint64_t bytes_pending_;
+  std::map<uint64_t, std::string> pending_data_;
 };
